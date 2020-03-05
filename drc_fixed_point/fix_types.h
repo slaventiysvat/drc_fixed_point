cb4fddddfefca76b6c3557f8917713797ba5a389
slaventiysvat <slaventiysvat@gmail.com>
2020-03-05 15:45:46 +0200

    changes

--- drc_fixed_point/fix_types.h
+++ drc_fixed_point/fix_types.h
@@ -1,1 +1,18 @@
 #pragma once
+
+typedef    long int                      FIX_32s;
+typedef    char                          FIX_8sig;
+typedef    unsigned int                  FIX_32uns;
+
+/*structure of accomulator register
+----------------------------------------------------------------------
+|    acc2 8bit guard bits |    acc1 32bit high   |   acc0 32bit low  |
+----------------------------------------------------------------------
+*/
+typedef struct
+{
+    FIX_8sig acc2 = 0;
+    FIX_32s acc1 = 0;
+    FIX_32s acc0 = 0;
+
+} FIX_72;
