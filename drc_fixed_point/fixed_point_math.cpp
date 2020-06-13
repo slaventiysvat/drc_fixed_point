6d9a704c0bf111f0f174f50eb87e3d1702d6fe13
Sviatoslav Starokozhev <sviatoslav.starokozhev@p-product.com>
2020-06-13 21:52:58 +0300

    DRC Fixed point:DRC-FIX-2-add_fix_mul_instruction

--- drc_fixed_point/fixed_point_math.cpp
+++ drc_fixed_point/fixed_point_math.cpp
@@ -3,6 +3,11 @@
 
 FIX_32s fix_div(FIX_32s a, FIX_32s b);
 
+FIX_32s fix_mul(FIX_32s a, FIX_32s b) {
+
+    return a * b;
+}
+
 FIX_32s fix_mul_rnd(FIX_32s a, FIX_32s b);
 
 FIX_32s fix_add(FIX_32s a, FIX_32s b) {
