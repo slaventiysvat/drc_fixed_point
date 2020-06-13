c1807651b1f6d7d55e78d886541aa530b75be55c
Sviatoslav Starokozhev <sviatoslav.starokozhev@p-product.com>
2020-06-13 19:32:16 +0300

    add new files to local repo

---
+++ fixed_point_math.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "fix_types.h"
+
+FIX_32s fix_div(FIX_32s a, FIX_32s b);
+
+FIX_32s fix_mul_rnd(FIX_32s a, FIX_32s b);
+
+FIX_32s fix_add(FIX_32s a, FIX_32s b);
+
+FIX_32s fix_sub(FIX_32s a, FIX_32s b);
+
+FIX_32s fix_mul_sat(FIX_32s a, FIX_32s b);
+
+FIX_32s fix_add_sat(FIX_32s a, FIX_32s b);
+
+FIX_32s fix_sub_sat(FIX_32s a, FIX_32s b);
+
+FIX_72 acc_mul(FIX_32s a, FIX_32s b);
+
+FIX_72 acc_mul_add(FIX_72 acc, FIX_32s a, FIX_32s b);
+
+FIX_72 acc_mul_sub(FIX_72 acc, FIX_32s a, FIX_32s b);
+
+FIX_72 acc_ashift(FIX_72 acc, FIX_32s a, FIX_32s b);
+
+FIX_72 acc_shift(FIX_72 acc, FIX_32s a, FIX_32s b);
+
+FIX_32s ashift(FIX_32s a);
+
+FIX_32s shift(FIX_32s a);
+
+FIX_72 mov_acc0(FIX_72 acc, FIX_32s a);
+
+FIX_72 mov_acc1(FIX_72 acc, FIX_32s a);
+
+FIX_72 mov_acc2(FIX_72 acc, FIX_32s a);
+
+FIX_72 get_acc0(FIX_72 acc, FIX_32s a);
+
+FIX_72 get_acc1(FIX_72 acc, FIX_32s a);
+
+FIX_72 get_acc2(FIX_72 acc, FIX_32s a);
---
+++ switch_defines.h
@@ -0,0 +1,5 @@
+#pragma once
+#define PEAK_DETECTOR
+#define RMS_DETECTOR
+#define SMA_DETECTOR
+#define EMA_DETECTOR
